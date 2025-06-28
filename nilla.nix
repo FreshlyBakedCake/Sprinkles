e6a69960aa316b32dbbfbb09d8343e16d6b55c9d
Samuel Shuert <coded@freshlybakedca.ke>
2025-06-28 03:07:07 +0000

    init: Repositorty setup & initial crates

---
+++ nilla.nix
@@ -0,0 +1,74 @@
+let
+  pins = import ./npins;
+
+  nilla = import pins.nilla;
+in
+nilla.create ({ config }: {
+  config = {
+    inputs = {
+      fenix = {
+        src = pins.fenix;
+      };
+
+      nixpkgs = {
+        src = pins.nixpkgs;
+
+        settings = {
+          overlays = [
+            config.inputs.fenix.result.overlays.default
+          ];
+        };
+      };
+    };
+
+    packages.default = config.packages.sprinkles;
+    packages.sprinkles = {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      package = { fenix, makeRustPlatform, lib, installShellFiles, ... }:
+        let
+          toolchain = fenix.complete.toolchain;
+
+          manifest = (lib.importTOML ./Cargo.toml).package;
+
+          platform = makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+        in
+        platform.buildRustPackage {
+          meta.mainProgram = "sprinkles";
+          pname = manifest.name;
+          version = manifest.version;
+
+          src = ./.;
+
+          nativeBuildInputs = [ installShellFiles ];
+
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+    };
+
+    shells.default = config.shells.sprinkles;
+    shells.sprinkles = {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      shell = { mkShell, fenix, bacon, pkg-config, reuse, ... }:
+        mkShell {
+          packages = [
+            (fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+              "rust-analyzer"
+            ])
+            bacon
+            pkg-config
+            reuse
+          ];
+        };
+    };
+  };
+})
