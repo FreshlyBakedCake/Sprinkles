27345c8cf29690c3672c42f1cbbc2f3bac8a9a28
Skyler Grey <minion@freshlybakedca.ke>
2026-05-17 13:32:40 +0200

    build: add third import paths case
    There are actually three cases for importing nilla, so we need 3
    different paths to be checked for when evaluating...

--- dependencies/nilla/default.nix
+++ dependencies/nilla/default.nix
@@ -1,5 +1,12 @@
 let
-  pins = if builtins.pathExists ./npins then import ./npins else import ../npins;
+  pins = (
+    if builtins.pathExists ./npins then
+      import ./npins # When we are accessing this in the nilla project
+    else if builtins.pathExists ../npins then
+      import ../npins # When we are accessing this in the toplevel project
+    else
+      import ../../npins # When we are accessing this as a dependency for another project
+  );
 
   lib = import pins.lib;
 in
